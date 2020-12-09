e77196ed07543bb9bf18e2580ce04a9f0bb6b0c8
Alessandro Ferraro <ale_ferraro@libero.it>
2020-12-09 16:50:22 +0100

    Section 1
    Add section 1

--- js/main.js
+++ js/main.js
@@ -10,6 +10,28 @@ const myApp = new Vue({
       'Features',
       'Blog',
       'Shop'
+    ],
+    sectionOneCards: [
+      {
+        imgUrl: 'img/home-5-image-box-01.png',
+        title: 'idea discussion',
+        text: 'Get teamed up with the specialists who work and teach codng for years at famous universities.'
+      },
+      {
+        imgUrl: 'img/home-5-image-box-02.png',
+        title: 'web development',
+        text: 'Learn to start building a webpage from scratch. You decide ypur own pace, course and speed.'
+      },
+      {
+        imgUrl: 'img/home-5-image-box-03.png',
+        title: 'system administration',
+        text: 'Learners are encouraged to study the mechanism and structure of system administration.'
+      },
+      {
+        imgUrl: 'img/home-5-image-box-04.png',
+        title: 'graphic design',
+        text: 'Have a passion for graphics and arts? Show your tam+lents with confidence and self-assertiveness.'
+      }
     ]
   },
   methods: {
