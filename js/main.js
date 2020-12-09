5d24a79c0708b149de508d117f8c9fb4ff5c6897
Alessandro Ferraro <ale_ferraro@libero.it>
2020-12-09 16:10:55 +0100

    Add Header
    Add Navbar and Jumbotron

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@
 const myApp = new Vue({
   el: "#root",
   data: {
-
+    navMenu: [
+      'Home',
+      'Pages',
+      'Courses',
+      'Features',
+      'Blog',
+      'Shop'
+    ]
   },
   methods: {
 
